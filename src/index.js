8a1d7c2d4be97b6a57fbeea4d7093a96bc99397a
fa3ze <118280320+fa3ze@users.noreply.github.com>
2022-11-21 00:37:07 +0330

    added relevant icon from the API

--- src/index.js
+++ src/index.js
@@ -35,6 +35,12 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "5c33c2674a0cd16815872b2fdd978d22";
